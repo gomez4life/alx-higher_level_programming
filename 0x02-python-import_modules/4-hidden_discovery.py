166ba7d99ec081fe517d6612a2b458a0aeb649e2
Gomina Peter <111024729+gomez4life@users.noreply.github.com>
2022-12-02 03:30:23 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
-    import hidden_4
+# This program prints all names defined in a compiled module hidden_4.pyc
 
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+if __name__ == '__main__':
+    import hidden_4
+    for i in dir(hidden_4):
+        if i[:2] != "__":
+            print("{}".format(i))
